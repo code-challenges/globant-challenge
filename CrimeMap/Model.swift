be8e9237405e30ca377ba3ba3e677686f2a0fc2a
Julio Guzman <julio.guzman@globant.com>
2015-12-17 13:36:11 -0600

    Make event's properties optional

--- CrimeMap/Model.swift
+++ CrimeMap/Model.swift
@@ -23,31 +23,31 @@ struct Location {
 }
 
 public class Event : NSObject {
-    var address = NSString()
-    var category = NSString()
-    var date = NSString()
-    var dayOfTheWeek = NSString()
-    var incidentNumber = NSString()
+    var address : NSString?
+    var category : NSString?
+    var date : NSString?
+    var dayOfTheWeek : NSString?
+    var incidentNumber : NSString?
     var location = Location()
-    var pddistrict = NSString()
-    var resolution = NSString()
-    var time = NSString()
+    var pddistrict : NSString?
+    var resolution : NSString?
+    var time : NSString?
     var x = Double()
     var y = Double()
     
     public init(dictionary: NSDictionary) {
-        self.address = dictionary["address"] as! NSString
-        self.category = dictionary["category"] as! NSString
-        self.date = dictionary["date"] as! NSString
-        self.dayOfTheWeek = dictionary["dayofweek"] as! NSString
-        self.incidentNumber = dictionary["incidntnum"] as! NSString
+        self.address = dictionary["address"] as? NSString
+        self.category = dictionary["category"] as? NSString
+        self.date = dictionary["date"] as? NSString
+        self.dayOfTheWeek = dictionary["dayofweek"] as? NSString
+        self.incidentNumber = dictionary["incidntnum"] as? NSString
         self.location = Location()
         self.location.latitude = dictionary["location"]!["latitude"] as! NSString
         self.location.longitude = dictionary["location"]!["longitude"] as! NSString
         self.location.needsRecording = dictionary["location"]!["needs_recoding"] as! Bool
-        self.pddistrict = dictionary["pddistrict"] as! NSString
-        self.resolution = dictionary["resolution"] as! NSString
-        self.time = dictionary["time"] as! NSString
+        self.pddistrict = dictionary["pddistrict"] as? NSString
+        self.resolution = dictionary["resolution"] as? NSString
+        self.time = dictionary["time"] as? NSString
         self.x = Double(dictionary["x"] as! String)!
         self.y = Double(dictionary["y"] as! String)!
         super.init()
